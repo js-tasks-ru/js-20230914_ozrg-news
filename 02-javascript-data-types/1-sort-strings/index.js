610548b0a7eb00eba4082c94d76b27b7e9bffb6d
Vladimir Blake <mcpebyjohan@gmail.com>
2023-09-21 16:18:40 +0300

    Fixed problems solutions from HW2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -7,28 +7,11 @@
 export function sortStrings(arr, param = 'asc') {
   let arrCopy = [...arr];
 
-  arrCopy.sort(compareFunction);
+  arrCopy.sort((a, b) => a.localeCompare(b, ["ru", "en"], {caseFirst: "upper"}));
 
   if (param === 'desc') {
     arrCopy = arrCopy.reverse();
   }
 
   return arrCopy;
-}
-
-function compareFunction(a, b) {
-  let minLength = Math.min(a.length, b.length);
-  for (let i = 0; i < minLength; i++) {
-    if (a[i] === b[i]) {
-      continue;
-    }
-
-    if (a[i].toLowerCase() === b[i].toLowerCase()) {
-      return (a[i].localeCompare(b[i]) === -1) ? 1 : -1;
-    }
-
-    return (a[i].toLowerCase()).localeCompare(b[i].toLowerCase());
-  }
-
-  return 0;
 }
