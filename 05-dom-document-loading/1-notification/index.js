4e6b991559f5b8d49d98d51fc5f01aa16ae2f909
Vladimir Blake <mcpebyjohan@gmail.com>
2023-10-02 19:02:23 +0300

    Solve task 1 from HW5 and partially task 2

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,46 @@
 export default class NotificationMessage {
+  static shown = false;
 
+  constructor (message, {duration = 1000, type = "success"} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+
+    this.element = this.element.firstElementChild;
+  }
+
+  show(appendingToElement = document.body) {
+    if (!NotificationMessage.shown) {
+      NotificationMessage.shown = true;
+      appendingToElement.append(this.element);
+
+      setTimeout(() => this.remove(), this.duration);
+    }
+  }
+
+  remove() {
+    NotificationMessage.shown = false;
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,44 @@
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+  }
 
+  render() {
+    this.element = document.createElement('div');
+    this.element.innerHTML = `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.getTableHeaderCellsHTML()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getTableHeaderCellsHTML() {
+    return this.headerConfig.map(columnData => {
+      return `
+        <div
+          class="sortable-table__cell"
+          data-id="${columnData.id}"
+          data-sortable="${columnData.sortable}"
+          data-order="${this.sortType}"
+        >
+          <span>${columnData.title}</span>
+          ${this.sortField === columnData.id ? this.getArrowHTML : ""}
+        </div>
+      `;
+    });
+  }
+
+  getArrowHTML() {
+    return `
+      <span data-element="arrow" class="sortable-table__sort-arrow">
+        <span class="sort-arrow"></span>
+      </span>
+    `;
   }
 }
 
