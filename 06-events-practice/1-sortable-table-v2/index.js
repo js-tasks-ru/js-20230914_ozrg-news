54939c9fab05d7a06c1e40224a08aecc303aa4d0
Vladimir Blake <mcpebyjohan@gmail.com>
2023-10-04 16:04:36 +0300

    Solve task 1 from HW6

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,204 @@
 export default class SortableTable {
   constructor(headersConfig, {
     data = [],
-    sorted = {}
+    sorted = {},
+    isSortLocally = true
   } = {}) {
+    this.headersConfig = this.prepareHeaderConfig(headersConfig);
+    this.data = data;
+    this.sortField = sorted.id;
+    this.sortOrder = sorted.order;
+    this.isSortLocally = isSortLocally;
+    this.element = document.createElement('div');
+    this.handleHeaderClick = this._handleHeaderClick.bind(this);
 
+    this.sort(this.sortField, this.sortOrder);
+  }
+
+  prepareHeaderConfig(headerConfig) {
+    let configeredHeader = new Map();
+    for (const headerColumn of headerConfig) {
+      configeredHeader.set(headerColumn.id, {
+        title: headerColumn.title,
+        sortable: headerColumn.sortable,
+        template: headerColumn.template,
+        sortType: headerColumn.sortType,
+      });
+    }
+
+    return configeredHeader;
+  }
+
+  _handleHeaderClick(e) {
+    let headerCell = this.getHeaderCellOnClick(e.target);
+
+    this.sortField = headerCell.getAttribute("data-id");
+
+    let sortingFieldConfig = this.headersConfig.get(this.sortField);
+    if (sortingFieldConfig.sortable) {
+      this.sortOrder = this.getNameOfReversedSortingOrder(this.sortOrder);
+    }
+
+    this.sort(this.sortField, this.sortOrder);
+  }
+
+  getHeaderCellOnClick(eventTarget) {
+    if (eventTarget.hasAttribute("data-id")) {
+      return eventTarget;
+    } else if (eventTarget.tagName === "SPAN") {
+      return eventTarget.parentElement;
+    }
+  }
+
+  getNameOfReversedSortingOrder(sortOrder) {
+    return (sortOrder === 'asc') ? 'desc' : 'asc';
+  }
+
+  createListeners() {
+    this.subElements.header.addEventListener('pointerdown', this.handleHeaderClick);
+  }
+
+  destroyListeners() {
+    this.subElements.header.removeEventListener('pointerdown', this.handleHeaderClick);
+  }
+
+  render() {
+    this.element.innerHTML = this.createTemplate();
+
+    this.element = this.element.firstElementChild;
+    this.subElements = this.element.querySelectorAll('div[data-element]');
+    this.subElements = {
+      header: this.subElements[0],
+      body: this.subElements[1],
+    };
+
+    this.createListeners();
+  }
+
+  createTemplate() {
+    return `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.createTableHeaderCellsHTML()}
+          </div>
+          <div data-element="body" class="sortable-table__body">
+            ${this.createTableRowsHTML()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  sort(fieldValue, orderValue) {
+    if (this.isSortLocally) {
+      this.sortOnClient(fieldValue, orderValue);
+    } else {
+      this.sortOnServer();
+    }
+  }
+
+  sortOnClient(fieldValue, orderValue) {
+    this.sortField = fieldValue;
+    this.sortOrder = orderValue;
+
+    let sortingFieldConfig = this.headersConfig.get(this.sortField);
+
+    if (sortingFieldConfig.sortable) {
+      let sortType = sortingFieldConfig.sortType;
+      let comparisonFunction = this.getComparisonFunction(
+        this.sortField,
+        sortType,
+        this.sortOrder
+      );
+      this.data.sort(comparisonFunction);
+      this.render();
+    }
+  }
+
+  sortOnServer() {
+
+  }
+
+  getComparisonFunction(sortField, sortType, sortOrder) {
+    if (sortOrder === 'asc') {
+      if (sortType === "string") {
+        return (a, b) => this.compareStringsAsc(a[sortField], b[sortField]);
+      } else {
+        return (a, b) => a[sortField] - b[sortField];
+      }
+    } else if (sortOrder === 'desc') {
+      if (sortType === "string") {
+        return (a, b) => this.compareStringsDesc(a[sortField], b[sortField]);
+      } else {
+        return (a, b) => b[sortField] - a[sortField];
+      }
+    }
+  }
+
+  compareStringsAsc(a, b) {
+    return a.localeCompare(b, ["ru", "en"], {caseFirst: "upper"});
+  }
+
+  compareStringsDesc(a, b) {
+    return b.localeCompare(a, ["ru", "en"], {caseFirst: "upper"});
+  }
+
+  createTableHeaderCellsHTML() {
+    let row = "";
+    this.headersConfig.forEach((properties, id) => {
+      row +=
+        `
+        <div
+          class="sortable-table__cell"
+          data-id="${id}"
+          data-sortable="${properties.sortable}"
+          data-order=${this.sortOrder}
+        >
+          <span>${properties.title}</span>
+          ${this.sortField === id ? this.createArrowHTML() : ""}
+        </div>
+        `;
+    });
+    return row;
+  }
+
+  createArrowHTML() {
+    return `
+      <span data-element="arrow" class="sortable-table__sort-arrow">
+        <span class="sort-arrow"></span>
+      </span>
+    `;
+  }
+
+  createTableRowsHTML() {
+    return this.data.map(rowContent => `
+      <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
+        ${this.createTableRowHTML(rowContent)}
+      </a>
+    `).join('');
+  }
+
+  createTableRowHTML(rowContent) {
+    let row = "";
+    this.headersConfig.forEach((properties, id) => {
+      let cellData = rowContent[id];
+      row += properties.template
+        ? properties.template(cellData)
+        :
+        `
+          <div class="sortable-table__cell">${cellData}</div>
+        `;
+    });
+    return row;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.destroyListeners();
+    this.remove();
   }
 }
